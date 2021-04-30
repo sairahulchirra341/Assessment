6c541fe0bf9038689162b4774762508f72087eeb
Sai Rahul Chirra <82822476+sairahulchirra341@users.noreply.github.com>
2021-04-30 10:25:36 +0530

    Create reverseSentence.py

---
+++ reverseSentence.py
@@ -0,0 +1,25 @@
+def reverse_word(s,start,end):
+    while start<end:
+        s[start],s[end]=s[end],s[start]
+        start=start+1
+        end-=1
+s=input("Enter any sentence:")
+s=list(s)
+start=0
+while True:
+    try:
+        end=s.index(' ',start)
+        reverse_word(s,start,end-1)
+        start=end+1
+    except ValueError:
+        reverse_word(s,start,len(s)-1)
+        break
+s.reverse()
+s="".join(s)
+print(s)
+
+
+Output:
+Enter any sentence: I like to join
+
+join to like I
